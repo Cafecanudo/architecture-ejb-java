44fce4e00936cd50903828bbf0e1389c3f55d2ba
Wellton S. Barros <welltonbarros@softbox.com.br>
2018-05-18 17:46:44 -0300

    save

---
+++ softbox-elan-web/src/src/@core/utils/ReactUtils.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function childrenWithProps(children, props) {
+  return React.Children.map(props.children, child => {
+    React.cloneElement(child, {...props})
+  })
+}
